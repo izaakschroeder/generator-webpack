c4e5170c016748c5c1ee4fa4e8f7ccac768562b0
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-09-19 20:55:56 -0700

    Make sass externals work like postcss ones.

--- generators/sass/templates/sass.webpack.config.js
+++ generators/sass/templates/sass.webpack.config.js
@@ -2,7 +2,10 @@
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import { NormalModuleReplacementPlugin } from 'webpack';
 
-export default function sass(options) {
+export default function sass({ target }) {
+	const env = process.env['NODE_ENV'] || 'development';
+	const external = env !== 'development';
+
 	return {
 		// Module settings.
 		module: {
@@ -17,21 +20,19 @@ export default function sass(options) {
 					'css',
 					'autoprefixer?browsers=last 2 version',
 					'font',
-					// 'icon', TODO: Enable me :D
 					'sass?precision=10&outputStyle=expanded&sourceMap=true'
 				]
 			}]
 		},
 
-		plugins: [
+		plugins: target === 'node' ? [
+			// Ignore CSS far as server-side goes
+			new NormalModuleReplacementPlugin(/\.(scss|sass)$/, 'node-noop')
+		] : (external ? [
 			// Some crawlers or things with Javascript disabled prefer normal CSS
 			// instead of Javascript injected CSS, so this plugin allows for the
 			// collection of the generated CSS into its own file.
-			new ExtractTextPlugin('[name].[hash].css'),
-
-			// Ignore CSS far as server-side goes
-			// TODO: How to enforce this only on server-side?
-			new NormalModuleReplacementPlugin(/\.s?css$/, 'node-noop')
-		]
+			new ExtractTextPlugin('[name].[contenthash].css')
+		] : [ ])
 	};
 }
