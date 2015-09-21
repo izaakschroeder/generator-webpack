553a24bfe82473145a7012cc25b24fb6e3e591ce
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-09-20 18:01:58 -0700

    Add support for `sharp-loader`.
    For fancy image processing.

---
+++ generators/sharp/index.js
@@ -0,0 +1,13 @@
+
+var generators = require('yeoman-generator');
+var util = require('yeoman-util');
+
+module.exports = generators.Base.extend({
+	writing: {
+		config: util.copy(
+			'~config/webpack/partial/sharp.webpack.config.js',
+			'sharp.webpack.config.js'
+		),
+		manifest: util.manifest()
+	}
+});
